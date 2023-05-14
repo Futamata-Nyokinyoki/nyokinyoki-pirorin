63144e4b9bd77b033318268acdeb6e5eb9549c29
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-05-15 00:09:11 +0900

    fix: adjust showing message when running

--- src/main/java/com/nyokinyoki/App.java
+++ src/main/java/com/nyokinyoki/App.java
@@ -20,17 +20,25 @@ public class App {
         LineReader reader = new UtilizedTerminal().getReader();
         List<Pattern> patterns = UtilizedTerminal.getAllOptions().stream()
                 .map(option -> Pattern.compile(option.getPattern())).collect(Collectors.toList());
+
         try {
             while (true) {
                 String line = reader.readLine("=====> ");
                 Matcher match;
+
+                boolean success = false;
                 for (int i = 0; i < patterns.size(); i++) {
                     Pattern p = patterns.get(i);
                     match = p.matcher(line);
                     if (match.matches()) {
                         hoge(i, match);
+                        success = true;
                         break;
                     }
+                }
+                if (!success) {
+                    alert("コマンドが不正確です。");
+                    UtilizedTerminal.helpMessage();
                 }
             }
         } catch (UserInterruptException e) {
@@ -40,80 +48,82 @@ public class App {
 
     static void hoge(int i, Matcher match) {
         int groupCount = match.groupCount();
-        System.out.println("i: " + i);
         switch (i) {
         case 0: {
-            System.out.println("Show Registered Courses");
+            System.out.println("登録済みの講義を表示します。");
             showRegisteredCourses();
             break;
         }
         case 1: {
-            System.out.println("Get Available Courses");
+            System.out.println("履修可能な講義を表示します。");
             getAvailableCourses();
             break;
         }
         case 2: {
-            System.out.println("Get Available Courses By Time Slot");
+            System.out.println("履修可能な講義のうち、指定した時間にあるものを表示します。");
             if (groupCount <= 1) {
                 System.out.println("getAvailableCourses: Invalid");
                 return;
             }
             int dayOfWeek = Integer.parseInt(match.group(1));
             int beginPeriod = Integer.parseInt(match.group(2));
-            System.out.println("Day of week: " + dayOfWeek);
-            System.out.println("Beginning Period: " + beginPeriod);
+            System.out.println("日付: " + dayOfWeek);
+            System.out.println("開始時刻: " + beginPeriod);
             getAvailableCoursesByTimeSlot(dayOfWeek, beginPeriod);
             break;
         }
         case 3: {
-            System.out.println("Add Course");
+            System.out.println("履修登録をします");
             if (groupCount <= 0) {
                 System.out.println("Add Course: Invalid Input");
             }
             int id = Integer.parseInt(match.group(1));
-            System.out.println("Course ID: " + id);
+            System.out.println("コースID: " + id);
             addCourse(id);
             break;
         }
         case 4: {
-            System.out.println("Remove Course");
+            System.out.println("履修登録を解除します");
             if (groupCount <= 0) {
                 System.out.println("Remove Course: Invalid Input");
             }
             int id = Integer.parseInt(match.group(1));
-            System.out.println("Course ID: " + id);
+            System.out.println("コースID: " + id);
             removeCourse(id);
             break;
         }
         case 5: {
-            System.out.println("Stamp");
+            System.out.println("打刻をします");
             if (groupCount <= 0) {
                 System.out.println("Stamp: Invalid Input");
             }
             String timestampString = match.group(1);
+            System.out.println("時刻: " + timestampString);
             stamp(timestampString);
             break;
         }
         case 6: {
-            System.out.println("Show All Stamp History");
+            System.out.println("すべての打刻を表示します");
             showAllStampHistory();
             break;
         }
         case 7: {
-            System.out.println("Show Attend History By Date");
+            System.out.println("指定した日付のすべての打刻を表示します");
             if (groupCount <= 0) {
                 System.out.println("Invalid Input");
             }
             String dateString = match.group(1);
+            System.out.println("日付: " + dateString);
             showAttendHistoryByDate(dateString);
             break;
         }
         case 8: {
-            System.out.println("Show Attend History By Course");
+            System.out.println("指定した講義の打刻を表示");
             if (groupCount <= 0) {
                 System.out.println("Invalid Input");
             }
             int id = Integer.parseInt(match.group(1));
+            System.out.println("コースID: " + id);
             showAttendHistoryByCourse(id);
             break;
         }
@@ -184,5 +194,9 @@ public class App {
         for (AttendStatus status : attendStatuses) {
             System.out.println(status);
         }
+    }
+
+    private static void alert(String sentence) {
+        System.out.println("\u001b[00;31m" + sentence + "\u001b[00m");
     }
 }
--- src/main/java/com/nyokinyoki/Terminal/UtilizedTerminal.java
+++ src/main/java/com/nyokinyoki/Terminal/UtilizedTerminal.java
@@ -3,19 +3,16 @@ package com.nyokinyoki.Terminal;
 import org.jline.builtins.Completers.RegexCompleter;
 import org.jline.console.*;
 import org.jline.reader.*;
-import org.jline.reader.Completer;
 import org.jline.reader.impl.DefaultParser;
 import org.jline.reader.impl.completer.StringsCompleter;
 import org.jline.terminal.*;
 import org.jline.utils.AttributedString;
 import org.jline.widget.TailTipWidgets.TipType;
-
 import org.jline.widget.TailTipWidgets;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.*;
 
 public class UtilizedTerminal {
     private static final String syllabus = "syllabus";
@@ -23,51 +20,8 @@ public class UtilizedTerminal {
 
     private static Map<String, CmdDesc> tailTips = new HashMap<>();
 
-    private static List<CLIArg> syllabusOptions = new ArrayList<>();
-    private static List<CLIArg> stampOptions = new ArrayList<>();
-
-    static {
-        String showRegisteredCoursesArg = "--show-registered-courses";
-        String getAvailableCoursesArg = "--get-available-courses";
-        String getAvailableCoursesByTimeSlotArg = "--get-available-courses-by-time-slot--day-of-week@<DAY_OF_WEEK>--beginning-period@<BEGINNIG_PERIOD>";
-        String addCourseArg = "--add-course@<LECTURE_ID>";
-        String removeCourseArg = "--remove-course@<LECTURE_ID>";
-        String stampArg = "--time@<yyyy-MM-dd-HH:mm:ss>";
-        String showAllStampHistoryArg = "--show-all-stamp-history";
-        String showAttendHistoryByDateArg = "--show-attend-history-by-date@<yyyy-MM-dd>";
-        String showAttendHistoryByCourseArg = "--show-attend-history-by-course@<COURSE_ID>";
-
-        CLIArg showRegisteredCoursesCLI = new CLIArg("登録済みの授業の表示", showRegisteredCoursesArg,
-                "\s*syllabus\s+--show-registered-courses\s*");
-        CLIArg getAvailableCoursesCLI = new CLIArg("登録可能な授業の取得", getAvailableCoursesArg,
-                "\s*syllabus\s+--get-available-courses\s*");
-        CLIArg getAvaliableCoursesByTimeSlotCLI = new CLIArg("<DAY_OF_WEEK>曜日(月曜：1, 金曜：5)の<BEGINNING_PERIOD>コマにある授業を表示",
-                getAvailableCoursesByTimeSlotArg,
-                "\s*syllabus\s+--get-available-courses-by-time-slot--day-of-week@([1-5])--beginning-period@([1-5])\s*");
-        CLIArg addCourseCLI = new CLIArg("<LECTURE_ID>がIDの講義を履修登録", addCourseArg,
-                "\s*syllabus\s+--add-course@([0-9]+)\s*");
-        CLIArg removeCourseCLI = new CLIArg("<LECTURE_ID>がIDを履修解除", removeCourseArg,
-                "\s*syllabus\s+--remove-course@([0-9]+)\s*");
-        CLIArg stampCLI = new CLIArg("<yyyy-MM-dd-HH:mm:ss>の時刻に打刻をする", stampArg,
-                "\s*stamp\s+--time@(\\d{4}-\\d{2}-\\d{2}-\\d{2}:\\d{2}:\\d{2})\s*");
-        CLIArg showAllStampHistoryCLI = new CLIArg("すべての打刻を表示", showAllStampHistoryArg,
-                "\s*stamp\s+--show-all-stamp-history-arg\s*");
-        CLIArg showAttendHistoryByDateCLI = new CLIArg("<yyyy-MM-dd>のすべての打刻を表示", showAttendHistoryByDateArg,
-                "\s*stamp\s+--show-attend-history-by-date@([0-9]{4}-[0-9]{2}-[0-9]{2})\s*");
-        CLIArg showAttendHistoryByCourseCLI = new CLIArg("<COURSE_ID>の講義のすべての打刻を表示", showAttendHistoryByCourseArg,
-                "\s*stamp\s+--show-attend-history-by-course@([0-9]+)");
-
-        syllabusOptions.add(showRegisteredCoursesCLI);
-        syllabusOptions.add(getAvailableCoursesCLI);
-        syllabusOptions.add(getAvaliableCoursesByTimeSlotCLI);
-        syllabusOptions.add(addCourseCLI);
-        syllabusOptions.add(removeCourseCLI);
-
-        stampOptions.add(stampCLI);
-        stampOptions.add(showAllStampHistoryCLI);
-        stampOptions.add(showAttendHistoryByDateCLI);
-        stampOptions.add(showAttendHistoryByCourseCLI);
-    }
+    private static List<CLIArg> syllabusOptions = Options.getSyllabusOptions();
+    private static List<CLIArg> stampOptions = Options.getStampOptions();
 
     private LineReader reader;
     private Completer completer;
@@ -117,12 +71,7 @@ public class UtilizedTerminal {
         List<String> syllabusDescList = syllabusOptions.stream().map(option -> option.getArg())
                 .collect(Collectors.toList());
 
-        Map<String, Completer> comp = new HashMap<>();
-        comp.put("C1", new StringsCompleter("stamp"));
-        comp.put("C11", new StringsCompleter(stampDescList.toArray(new String[0])));
-        comp.put("C2", new StringsCompleter("syllabus"));
-        comp.put("C21", new StringsCompleter(syllabusDescList.toArray(new String[0])));
-
+        Map<String, Completer> comp = setCompleter(stampDescList, syllabusDescList);
         tailTips.put(stamp, new CmdDesc(stampMainDesc, ArgDesc.doArgNames(stampDescList), stampWidgetOpts));
         tailTips.put(syllabus, new CmdDesc(syllabusMainDesc, ArgDesc.doArgNames(syllabusDescList), syllabusWidgetOpts));
         return new RegexCompleter("C1 C11? | C2 C21?", comp::get);
@@ -132,5 +81,25 @@ public class UtilizedTerminal {
         System.out.println("打刻関連: stamp");
         System.out.println("履修関連: syllabus");
         System.out.println("Please press TAB key to continue");
+    }
+
+    private static Map<String, Completer> setCompleter(List<String> stampDescList, List<String> syllabusDescList) {
+        Map<String, Completer> completer = new HashMap<>();
+        completer.put("C1", new StringsCompleter("stamp"));
+        completer.put("C11", new StringsCompleter(stampDescList.toArray(new String[0])));
+        completer.put("C2", new StringsCompleter("syllabus"));
+        completer.put("C21", new StringsCompleter(syllabusDescList.toArray(new String[0])));
+        return completer;
+    }
+
+    public static void helpMessage() {
+        System.out.println("実行可能コマンド");
+
+        for (CLIArg cmd : syllabusOptions) {
+            System.out.printf("syllabus %-50s %s\n", cmd.getArg(), cmd.getDesc());
+        }
+        for (CLIArg cmd : stampOptions) {
+            System.out.printf("stamp    %-50s %s\n", cmd.getArg(), cmd.getDesc());
+        }
     }
 }
